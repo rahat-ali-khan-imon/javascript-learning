15064555f39f49cf5b8579d30635929953ff3bda
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-24 18:30:13 +0600

    Add data types and type conversion examples in JavaScript

---
+++ 01-basics/p03_datatype.js
@@ -0,0 +1,29 @@
+var i = 1234;
+var f = 45.67;
+var number = Number(34);
+let n = Number('45');       // string to number convert
+
+console.log(number);
+console.log(n);
+console.log(Number.parseInt(45.45));
+console.log(Number.parseFloat(57));
+console.log();
+
+console.log(Number.MAX_VALUE);
+console.log(Number.MAX_SAFE_INTEGER);
+// console.log(Number.MIN_VALUE);
+console.log(10 / 0);
+console.log('abe' * 10);
+console.log();
+
+
+var str1 = 'String';
+var str2 = "String";
+var str3 = `String`;
+
+var str4 = String('aaaaaaaasd');
+var str5 = String(12345);
+var str6 = String(3.1416);      // String constructor
+
+console.log(str1, str2, str3);
+console.log(str4, str5, str6);
