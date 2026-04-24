05a590e5d1401680822d46212aeeb99936d3e007
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-24 17:30:49 +0600

    Add basic console.log examples in JavaScript

---
+++ 01-basics/p01_hello_world.js
@@ -0,0 +1,8 @@
+console.log('Hello World!');
+console.log('I love programming.');
+console.log(123454);
+console.log(60.55);
+
+console.log('My fav no: ' + 9);
+console.log(9 + 9); 
+console.log('9' + 9);
