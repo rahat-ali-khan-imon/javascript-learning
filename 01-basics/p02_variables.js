c29ff9e94df4915e9348e8f20b84f6dbb824fa30
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-24 17:55:15 +0600

    Add variable usage examples with console output

---
+++ 01-basics/p02_variables.js
@@ -0,0 +1,14 @@
+console.log('HM Nayem knows JavaScript.');
+console.log('His age is only 24');
+console.log('HM Nayem is creating a JavaScript course for all.');
+console.log('But his age is only 24');
+console.log();
+
+
+var name = 'Hasan Mahmud Nayem';
+var age = 23;
+
+console.log(name + ' knows JavaScript.');
+console.log('His age is only ' + age);
+console.log(name + ' is creating a JavaScript course for all.');
+console.log('But his age is only ' + age);
