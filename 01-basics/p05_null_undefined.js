76af5690ba153c545220674aaabcaed7b4fc0b84
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-30 02:27:47 +0600

    Demonstrate difference between null and undefined

--- 01-basics/p05_null_undefined.js
+++ 01-basics/p05_null_undefined.js
@@ -1,5 +1,6 @@
-let abc;
-let xyz = null;
+// Variable declared but NOT assigned
+let abc;            // default value = undefined
+let xyz = null;    // intentional empty value (no value)
 
 console.log(abc);
 console.log(xyz);
