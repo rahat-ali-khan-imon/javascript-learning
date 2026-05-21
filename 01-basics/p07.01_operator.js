4e892b3a850bcc18b409fd449caacd0593c16435
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-05-22 04:31:35 +0600

    Arithmetic operators and increment-decrement examples

---
+++ 01-basics/p07.01_operator.js
@@ -0,0 +1,26 @@
+// Arithmetic Operator
+
+let a = 10;
+let b = 3;
+
+console.log(a % b);
+console.log(a % 6);
+console.log();
+
+
+/** Increment & Decrement
+ * Pre Increment
+ * Post Increment
+*/
+
+console.log(a++);   // Post Increment
+console.log(a);
+console.log(++a);  // Pre Increment
+console.log(a);
+console.log();
+
+
+console.log(a--);   // Post Decrement
+console.log(a);
+console.log(--a);   // Pre Decrement
+console.log(a);
