86f1b36679d9f30fd22c8e8e0621a924dcc67760
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-05-21 17:09:17 +0600

    Boolean values and Boolean conversion

--- 01-basics/p04_boolean.js
+++ 01-basics/p04_boolean.js
@@ -1,12 +1,12 @@
 // Boolean primitive values
 let b1 = true;
-let b2 = false;     // direct boolean value (primitive)
+let b2 = false;    // direct boolean value (primitive)
 
 console.log(b1, b2);
 
 
 // Boolean constructor / conversion
 let b3 = Boolean(true);
-let b4 = Boolean(false);    // true → boolean true (no change)
+let b4 = Boolean(false);   // true → boolean true (no change)
 
 console.log(b3, b4);
