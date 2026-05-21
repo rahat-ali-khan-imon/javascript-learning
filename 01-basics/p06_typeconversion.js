7abb2b826452a5b6def78053771193a2aeb0886e
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-05-21 23:13:22 +0600

    Type conversion and number-string operations

---
+++ 01-basics/p06_typeconversion.js
@@ -0,0 +1,14 @@
+let str = '1000';
+let n = 25;
+
+console.log(str * n);       // Automatically type conversion
+console.log(str - n);
+console.log(str + n);
+console.log();
+
+console.log(Number(str));
+console.log(str);
+console.log(Number.parseInt(str));
+console.log();
+
+console.log(n.toString());
