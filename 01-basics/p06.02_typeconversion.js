e2fe0d3b0f3a886a90f86845e0f4b32d636ed506
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-05-22 03:00:05 +0600

    Falsy and truthy values in JavaScript

---
+++ 01-basics/p06.02_typeconversion.js
@@ -0,0 +1,17 @@
+/** Falsy Values
+ * 0
+ * null
+ * undefined
+ * NaN
+*/
+
+console.log(Boolean(''));
+console.log(Boolean('dsjkhfsdjkfhsdj'));
+console.log();
+
+console.log(Boolean(null));
+console.log(Boolean(undefined));
+console.log();
+
+console.log(Boolean(0));
+console.log(Boolean(123));
