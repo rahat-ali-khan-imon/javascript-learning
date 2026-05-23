147130de819bcacfda46f8eb65b48eec5d5757f1
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-05-24 00:40:12 +0600

    Assignment operators and shorthand examples

---
+++ 01-basics/p07.02_operator.js
@@ -0,0 +1,18 @@
+// Assignment Operator
+
+let a = 10;
+let b = 20;
+
+a = a + b;
+
+console.log('a = ', a);
+console.log('b = ', b);
+
+
+b += a;     // Shortcut
+console.log('b = ', b);
+console.log();
+
+
+a -= b;
+console.log('a = ', a);
