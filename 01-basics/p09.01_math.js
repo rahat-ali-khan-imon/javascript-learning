926db45e229e906afdb2e706700ecc3e6bd944d5
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-05-24 03:26:16 +0600

    JavaScript Math object methods and utilities

---
+++ 01-basics/p09.01_math.js
@@ -0,0 +1,31 @@
+console.log('Math.E = ', Math.E);
+console.log('Math.PI = ', Math.PI);
+console.log();
+
+
+let n = Math.abs(-4.389);
+
+console.log('Math.abs() = ', n);
+console.log('Math.floor() = ', Math.floor(n));
+console.log('Math.ceil() = ', Math.ceil(n));
+console.log('Math.round() = ', Math.round(n));
+console.log();
+
+
+console.log('Math.max() = ', Math.max(100, 400));
+console.log('Math.min() = ', Math.min(400, 100));
+console.log('Math.max() = ', Math.max(100, 400, 4000));
+console.log('Math.min() = ', Math.min(400, 100, -1000));
+console.log();
+
+
+console.log('Math.pow() = ', Math.pow(2, 3));
+console.log('Math.pow() = ', Math.pow(3, 5));
+console.log('Math.sqrt() = ', Math.sqrt(9));
+console.log('Math.sqrt() = ', Math.sqrt(64));
+console.log();
+
+
+console.log('Math.random() = ', Math.random());
+console.log('Math.round(Math.random() * 50 + 1) = ', Math.round(Math.random() * 50 + 1));
+console.log('Math.floor(Math.random() * 50) + 1 = ', Math.floor(Math.random() * 50) + 1);
