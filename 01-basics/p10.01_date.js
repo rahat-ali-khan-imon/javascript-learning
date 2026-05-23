e72561ebd723544642e60546587c8caf2f338577
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-05-24 04:20:40 +0600

    JavaScript Math object methods and utilities

---
+++ 01-basics/p10.01_date.js
@@ -0,0 +1,16 @@
+let date = new Date();
+console.log('new Date() = ', date);
+
+console.log('date.toDateString() = ', date.toDateString());
+console.log('date.toTimestring() = ', date.toTimeString());
+console.log('date.toLocaleString() = ', date.toLocaleString());
+console.log();
+
+
+console.log('date.getFullYear() = ', date.getFullYear());
+console.log('date.getMonth() = ', date.getMonth());
+console.log('date.getMonth() + 1 = ', date.getMonth() + 1);
+console.log('date.getDate() = ', date.getDate());
+console.log('date.getDay() = ', date.getDay());
+console.log('date.getHours() = ', date.getHours());
+console.log('date.getMinutes() = ', date.getMinutes());
