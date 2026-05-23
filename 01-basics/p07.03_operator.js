54dd3b71f8043d32aeaf28f7d821d9da8543b88b
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-05-24 01:13:13 +0600

    Comparison operators and equality examples

---
+++ 01-basics/p07.03_operator.js
@@ -0,0 +1,26 @@
+// Comparison Operators
+
+let a = 10;
+let b = 20;
+
+console.log(a == b);    // false
+console.log(a != b);   // true
+console.log();
+
+console.log(a > b);     // false
+console.log(a < b);    // true
+console.log();
+
+
+a = 20;
+
+console.log(a >= b);    // true 
+console.log(a <= b);   // true
+console.log();
+
+
+let c = '50';
+let d = 50;
+
+console.log(c == d);        // true 
+console.log(c === d);      // false
