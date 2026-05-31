7efa533e74dbaac268379106ad7847ac4e97b30c
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-05-31 23:46:47 +0600

    Practice if statements for comparison and odd-even checking

---
+++ 01-basics/p11.01_if.js
@@ -0,0 +1,21 @@
+let a = 10;
+let b = 20;
+
+if(a > b) {
+    console.log(a + ' is greater than ' + b);
+}
+
+if(a < b) {
+    console.log('B is greater than A');
+}
+
+
+let n = 5;
+
+if(n % 2 === 0) {
+    console.log(n + ' is Even Number');
+}
+
+if(n % 2 === 1) {
+    console.log(n + ' is Odd Number');
+}
